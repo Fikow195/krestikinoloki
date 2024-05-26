2eb88e1dddd2005e515cd0848464ec0b9200f01d
Prusikova <a.u.prusikova@gmail.com>
2024-05-26 18:22:55 +0300

    поле+наброски самой игры без проверок на победу

--- 1.cpp
+++ 1.cpp
@@ -21,12 +21,86 @@ public:
 			cout << endl;
 		}
 	}
+	void printField() {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				cout << field[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
 };
 
 
-class Player
+void Player(char A)
 {
-public:
 	char A;
+	cout << "Выберите x или o: " << endl;
+	cin >> A;
+	int x;
+	int x0;
+	int y0;
+	int y;
+	const int row = 3;
+	const int col = 3;
+	int field[3][3];
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			field[i][j] = '-';
+			cout << field[i][j] << " ";
+		}
+		cout << endl;
+	}
+	while (true)
+	{
+		cout << "Введите расположение по горизонтали " << endl;
+		cin >> x0;
+		x = x0 - 1;
+		cout << "Введите расположение по вертикали " << endl;
+		cin >> y0;
+		y = y0 - 1;
 
-};
+		for (int i = 0; i < 3; i++) {
+
+			if (field[x][y] == 'x') {
+				cout << "место занято" << endl;
+				break;
+				continue;
+
+			}
+			else if (field[x][y] == 'o') {
+				cout << "место занято" << endl;
+				continue;
+
+			}
+			for (int i = 0; i < row; i++)
+			{
+				for (int j = 0; j < col; j++)
+				{
+
+					cout << field[i][j] << " ";
+					field[x][y] = A;
+				}
+				cout << endl;
+			}
+		}
+	}
+	};
+
+	void main()
+	{
+		setlocale(LC_ALL, "ru");
+		char A;
+		int x;
+		int x0;
+		int y0;
+		int y;
+		const int row = 3;
+		const int col = 3;
+		int field[3][3];
+		
+		Player('x');
+		Player('o');
+	}
